e216f5cfdf30da064fe0fc0c198e5a3d40a4e9f0
Andraelity <andraelity@gmail.com>
2023-12-26 17:48:06 -0500

    FIRST COMMIT

---
+++ Assets/Shapes/CircleDebugRenderer.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+using WriteTxtLog;
+
+
+namespace Shapes.Samples.Circles
+{
+	
+	public class CircleDebugRenderer : MonoBehaviour {
+		
+		public Color FillColor;
+		public string Path;
+
+		[Range(1f, 10f)] public float Radius = 2f;
+
+
+		 
+		private void Update()
+		{
+
+			var circleInfo = new CircleInfo
+			{
+				center = transform.position,
+				forward = transform.forward,
+				radius = Radius,
+				fillColor = FillColor,
+				pathShader = Path
+			};
+			
+			
+			Circle.Draw(circleInfo);
+		}
+
+		// private void Start()
+		// {
+
+
+		// 	var circleInfo = new CircleInfo
+		// 	{
+		// 		center = transform.position,
+		// 		forward = transform.forward,
+		// 		radius = Radius,
+		// 		fillColor = FillColor
+		// 	};
+
+			
+		// 	Circle.Draw(circleInfo);
+
+		// 	Debug.Log(Quaternion.identity);
+
+		// }
+
+		void Awake()
+		{
+			// WriteTxtLogClass.FunctionWrite();
+		}
+
+	}
+}
---
+++ Assets/Shapes/CircleInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Shapes
+{
+	public struct CircleInfo
+	{
+		public float radius;
+		public Vector3 center;
+		public Vector3 forward;
+		public Color fillColor;
+		public string pathShader;
+	}
+}
